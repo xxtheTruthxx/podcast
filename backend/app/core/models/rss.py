47efa71b81780a2d1ed5e20124c117f8f03af0f4
xxtheTruthxx <ctrlramx@gmail.com>
2025-10-18 12:33:50 +0300

    feat: add new feed variables

--- backend/app/core/models/rss.py
+++ backend/app/core/models/rss.py
@@ -2,8 +2,13 @@
 from pydantic import BaseModel
 
 class RssFeed(BaseModel):
+    image_url: str
+    date: str
     title: str
     description: str
-    
+    author: str
+    origin: str
+    uuid: str
+
 class RssFeedPost(BaseModel):
     title: str

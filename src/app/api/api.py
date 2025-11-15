9de06bfa01c04008210ef8ec15ca53022043c269
xxtheTruthxxxxtheTruthxx <ctrlramx@gmail.com>
2025-11-15 13:21:34 +0200

    feat: include podcast api

--- src/app/api/api.py
+++ src/app/api/api.py
@@ -2,8 +2,10 @@
 from fastapi import APIRouter
 
 # Local Dependencies
+from core.config import settings
 from .routes import (
   podcast,
+  podcast_api,
   rss,
   admin
 )
@@ -13,5 +15,6 @@ api_router = APIRouter()
 
 # Include routers
 api_router.include_router(podcast.router, prefix="/podcast")
+api_router.include_router(podcast_api.router, prefix=f"{settings.API_V1_STR}/podcast")
 api_router.include_router(rss.router, prefix="/rss")
 api_router.include_router(admin.router)
